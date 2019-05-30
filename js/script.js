376c59ee6a8819d474a74c01c0543a7bbf5986c6
Aliaksei Pavarazniuk <stormur@rambler.ru>
2019-05-30 17:37:05 +0200

    prettify

--- js/script.js
+++ js/script.js
@@ -1,52 +1,51 @@
 $(document).ready(function() {
 
-// Dropdown
-$('.ui.dropdown')
-.dropdown({
-  hoverable: true,
-  on: 'hover',  
-  delay : {
-    show: 10,
-    hide: 500
-  }
+    // Dropdown
+    $('.ui.dropdown')
+        .dropdown({
+            hoverable: true,
+            on: 'hover',
+            delay: {
+                show: 10,
+                hide: 500
+            }
+        });
+
+    $('#utility-close').click(function() {
+        $('#utility').css('visibility', 'hidden');
+    });
+
+    $(".questions-block-card .plus").click(function() {
+        $(this).siblings('.questions-block-card p').slideToggle("500");
+        $(this).children('.questions-block-card .vertical').toggle();
+    });
+
+    $(".issues-click").click(function() {
+        $(this).siblings('.issues p').slideToggle("500");
+        $(this).children('.issues-click .arrow').toggleClass("arrow-down");
+    });
+
+    $('textarea').focus(function() {
+        $(this).data('placeholder', $(this).attr('placeholder'))
+        $(this).attr('placeholder', '');
+    });
+
+    $('textarea').blur(function() {
+        $(this).attr('placeholder', $(this).data('placeholder'));
+    });
+
+
+    (function($) {
+        $(function() {
+
+            $('ul.tabs').on('click', 'li:not(.active)', function() {
+                $(this)
+                    .addClass('active').siblings().removeClass('active')
+                    .closest('div#legal').find('div.tabs-content').removeClass('active').eq($(this).index()).addClass('active');
+            });
+
+        });
+    })($);
+
+
 });
-
-$('#utility-close').click(function () {
-  $('#utility').css('visibility','hidden');
-});
-
-  $(".questions-block-card .plus").click(function () {
-    $(this).siblings('.questions-block-card p').slideToggle("500");
-    $(this).children('.questions-block-card .vertical').toggle();  
-  });
-
-    $(".issues-click").click(function () {
-    $(this).siblings('.issues p').slideToggle("500");
-    $(this).children('.issues-click .arrow').toggleClass("arrow-down");  
-  });
-
-    $('textarea').focus(function(){
- $(this).data('placeholder',$(this).attr('placeholder'))
- $(this).attr('placeholder','');
-});
-
-$('textarea').blur(function(){
- $(this).attr('placeholder',$(this).data('placeholder'));
-});
-
-
-(function($) {
-$(function() {
-
-  $('ul.tabs').on('click', 'li:not(.active)', function() {
-    $(this)
-      .addClass('active').siblings().removeClass('active')
-      .closest('div#legal').find('div.tabs-content').removeClass('active').eq($(this).index()).addClass('active');
-  });
-
-});
-})($);
-
-
-});
-
